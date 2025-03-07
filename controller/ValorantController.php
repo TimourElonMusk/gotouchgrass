a4ad18e22c2663107550bc392b245cdb25568d35
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2025-03-07 15:25:21 +0100

    Update ValorantController.php

--- controller/ValorantController.php
+++ controller/ValorantController.php
@@ -1,1 +1,10 @@
+<?php 
+require_once('model/ValorantModel.php');
 
+class ValorantController {
+    public function showAgents() {
+        // Create a new instance $agentslist from the class ValorantModel
+        $
+    }
+}
+?>
