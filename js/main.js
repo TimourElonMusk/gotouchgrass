1736cbb3ff807cfa7dad1e4a8d3445e556354fe6
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2025-04-01 08:48:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+let isScrolling = false;
+
 document.getElementById('arrowDownBtn').addEventListener('click', () => {
     const startPosition = window.scrollY;
-    const distance = document.getElementById('features').getBoundingClientRect().top + window.scrollY - document.querySelector('header').offsetHeight - startPosition;
+    const distance = document.getElementById('categories').getBoundingClientRect().top + window.scrollY - document.querySelector('header').offsetHeight - startPosition;
     const duration = 1000;
-
     let startTime = null;
+
+    isScrolling = true;
 
     function easeInOutQuad(t) {
         return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
@@ -13,8 +16,11 @@ document.getElementById('arrowDownBtn').addEventListener('click', () => {
         if (!startTime) startTime = currentTime;
 
         const elapsedTime = currentTime - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
 
-        window.scrollTo(0, startPosition + distance * easeInOutQuad(Math.min(elapsedTime / duration, 1)));
+        if (!isScrolling) return;
+
+        window.scrollTo(0, startPosition + distance * easeInOutQuad(progress));
 
         if (elapsedTime < duration) {
             requestAnimationFrame(scrollAnimation);
@@ -23,3 +29,6 @@ document.getElementById('arrowDownBtn').addEventListener('click', () => {
 
     requestAnimationFrame(scrollAnimation);
 });
+
+window.addEventListener('wheel', () => isScrolling = false, { passive: true });
+window.addEventListener('touchstart', () => isScrolling = false, { passive: true });
