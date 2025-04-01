b58bc3f6c45a72d3746ed593ff269c4c05caabb1
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2025-04-01 11:39:51 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,13 @@
 <header>
-    <img src="<?= BASE_URL ?>/images/header.jpg" alt="Banner">
+    <div class="header__banner-container">
+        <img class="header__banner" src="<?= BASE_URL ?>/images/header.jpg" alt="Banner">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#"></a>Agents</li>
+            <li><a href="#"></a>Gamemodes</li>
+            <li><a href="#"></a>Maps</li>
+            <li><a href="#"></a>Weapons</li>
+        </ul>
+    </nav>
 </header>
