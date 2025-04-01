4744061bb059328a13d2665e3f4a6a7e9158e832
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2025-04-01 12:23:15 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,12 @@
         <br>
         <p class="footer__small-text">
             VALORANT DATA is a non-official data source and not endorsed by Riot Games in any way.
-            <br> Riot Games, Valorant, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
+            <br>
+            Riot Games, Valorant, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
         </p>
+
+        <ul class="footer__logos">
+            <li><img src="<?= BASE_URL ?>/images/github-brands.svg" alt="Github logo"></li>
+        </ul>
     </div>
 </footer>
