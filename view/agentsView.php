55ad6576159b2d64ba5413210afe90ae77127c9c
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2025-04-08 12:26:59 +0200

    Add files via upload

--- view/agentsView.php
+++ view/agentsView.php
@@ -1,3 +1,14 @@
+<?php function getAbilityBySlot(array $abilities, string $slot): ?array {
+    foreach ($abilities as $ability) {
+        if ($ability['slot'] === $slot) {
+            return $ability;
+        }
+    }
+    return null;
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,13 +26,27 @@
                     <div class="card">
                         <div class="card-inner">
                             <div class="card-front">
-                                <img class="fullPortraitV2" src="<?=$agent["fullPortraitV2"]?>" alt="Portrait">
+                                <img class="fullPortraitV2" src=<?=$agent["fullPortraitV2"]?> alt="Portrait">
                             </div>
                             <div class="card-back">
                                 <div class="agent-info">
                                     <h2><?=$agent["displayName"]?></h2>
-                                    <img class="role-img" src="<?=$agent["role"]["displayIcon"]?>" alt="Role Icon">
+                                    <img class="role-img" src=<?=$agent["role"]["displayIcon"]?> alt="Role Icon" title="<?=$agent["role"]["description"]?>">
                                     <p class="description"><?=$agent["description"]?></p>
+                                    <div class="abilities">
+                                        <?php $ability = getAbilityBySlot($agent["abilities"], "Ability1"); ?>
+                                        <div class="ability-container">
+                                            <img src="<?=$ability["displayIcon"]?>" alt="Ability 1", title="<?=$ability["displayName"]?> : <?=$ability["description"]?>">
+                                        </div>
+                                        <?php $ability = getAbilityBySlot($agent["abilities"], "Ability2"); ?>
+                                        <div class="ability-container">
+                                            <img src="<?=$ability["displayIcon"]?>" alt="Ability 2", title="<?=$ability["displayName"]?> : <?=$ability["description"]?>">
+                                        </div>
+                                        <?php $ability = getAbilityBySlot($agent["abilities"], "Grenade"); ?>
+                                        <div class="ability-container">
+                                            <img src="<?=$ability["displayIcon"]?>" alt="Grenade", title="<?=$ability["displayName"]?> : <?=$ability["description"]?>">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
